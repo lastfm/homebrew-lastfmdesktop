749c803298eaf84d578f0468719abea52237d2eb
Andrew Kevich <andrew.kevich@gmail.com>
2014-11-18 20:08:26 +0600

    Added support up to Yosemite version of MacOS by updating set of SDK path

--- vlc.rb
+++ vlc.rb
@@ -30,12 +30,9 @@ class Vlc < Formula
     # this is needed to find some m4 macros installed by homebrew's pkg-config 
     aclocal = "ACLOCAL_ARGS=\"-I /usr/local/share/aclocal\""
 
+
     if MacOS.xcode_version.to_f >= 4.3
-      if MacOS.mountain_lion?
-        sdk = "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.7.sdk"
-      else
-        sdk = "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.6.sdk"
-      end
+      sdk = "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX#{MacOS.version}.sdk"
     else
       sdk = "/Developer/SDKs/MacOSX10.6.sdk"
     end
